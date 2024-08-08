2ae23dad291228c5fa5beec1be9542fe72707591
emmascabbia19 <emma.scabbia19@gmail.com>
2024-08-08 16:32:28 +0200

    create prodottoDTO, scarpaDTO, vestitoDTO, repository, service interface and implementation of prodotto, vestito, scarpa

---
+++ backend/src/main/java/com/betacom/backend/dto/VestitoDTO.java
@@ -0,0 +1,45 @@
+package com.betacom.backend.dto;
+
+public class VestitoDTO {
+	
+	private Integer id;
+	private String taglia;
+	private String vestibilita;
+	private String lunghezza;
+	private String lunghezzaManica;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getTaglia() {
+		return taglia;
+	}
+	public void setTaglia(String taglia) {
+		this.taglia = taglia;
+	}
+	public String getVestibilita() {
+		return vestibilita;
+	}
+	public void setVestibilita(String vestibilita) {
+		this.vestibilita = vestibilita;
+	}
+	public String getLunghezza() {
+		return lunghezza;
+	}
+	public void setLunghezza(String lunghezza) {
+		this.lunghezza = lunghezza;
+	}
+	public String getLunghezzaManica() {
+		return lunghezzaManica;
+	}
+	public void setLunghezzaManica(String lunghezzaManica) {
+		this.lunghezzaManica = lunghezzaManica;
+	}
+	
+	
+
+}
